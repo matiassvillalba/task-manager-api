74c479dc55426d1b68e89573d59557902579c869
Matias <matiasvillalba2319@gmail.com>
2026-07-25 20:53:56 -0300

    feat: implement read task endpoints

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, status
+from fastapi import FastAPI, HTTPException, status
 
 from schemas import TaskCreate, TaskResponse
 
@@ -8,7 +8,6 @@ app = FastAPI(
     version="1.0.0",
 )
 
-# Base de datos en memoria (lista temporal para guardar las tareas)
 tasks_db: list[dict] = []
 task_id_counter = 1
 
@@ -22,16 +21,28 @@ def read_root():
 def create_task(task: TaskCreate):
     global task_id_counter
 
-    # Convertimos los datos validados por Pydantic a un diccionario de Python
     new_task = task.model_dump()
-
-    # Asignamos el ID único autoincremental
     new_task["id"] = task_id_counter
-
-    # Guardamos la nueva tarea en la lista
     tasks_db.append(new_task)
-
-    # Incrementamos el contador para la próxima tarea
     task_id_counter += 1
 
     return new_task
+
+
+@app.get("/tasks", response_model=list[TaskResponse], status_code=status.HTTP_200_OK)
+def get_tasks():
+    return tasks_db
+
+
+@app.get(
+    "/tasks/{task_id}", response_model=TaskResponse, status_code=status.HTTP_200_OK
+)
+def get_task(task_id: int):
+    for task in tasks_db:
+        if task["id"] == task_id:
+            return task
+
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"Task with ID {task_id} not found",
+    )
